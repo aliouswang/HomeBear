c2f63e04fa693e4170f6cbaf94ec1962dc4cd724
aliouswang <wsw622@126.com>
2017-11-27 17:18:04 +0800

    Initial commit

---
+++ app/src/main/java/com/hmzl/chinesehome/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hmzl.chinesehome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //https://juejin.im/entry/58f5e115a22b9d006c0ab28c
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ domain/src/main/java/com/hmzl/chinesehome/library/domain/myClass.java
@@ -0,0 +1,4 @@
+package com.hmzl.chinesehome.library.domain;
+
+public class myClass {
+}
