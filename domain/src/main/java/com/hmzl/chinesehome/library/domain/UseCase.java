2f1a4b5ffa84eae44307ccb59a62e7a956b4c90b
mac <wsw622@126.com>
2017-11-30 07:08:01 +0800

    init commit

--- domain/src/main/java/com/hmzl/chinesehome/library/domain/UseCase.java
+++ domain/src/main/java/com/hmzl/chinesehome/library/domain/UseCase.java
@@ -4,5 +4,15 @@ package com.hmzl.chinesehome.library.domain;
  * Created by mac on 2017/11/27.
  */
 
-public class UseCase {
+public abstract class UseCase {
+
+    public interface RequestValue {
+
+
+    }
+
+    public interface ResponseValue {
+
+    }
+
 }
